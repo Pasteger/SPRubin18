df2d32651cf9b8c2483dfcde6fa851b9b505f285
Дима <77545533+Pasteger@users.noreply.github.com>
2021-09-15 10:53:00 +0300

    Add files via upload

--- untitled2/src/it/college/Main.java
+++ untitled2/src/it/college/Main.java
@@ -1,15 +1,45 @@
 package it.college;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("Введите умножаемое число");
-	    double multiplier0 = scanner.nextInt();
-	    System.out.println("Введите на какое число будет умножаться");
-	    double multiplier1 = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(
+				"D:\\Programs\\JavaProject\\untitled2\\src\\it\\college\\txt.txt"));
+		String line;
+		StringBuilder text = new StringBuilder();
+		String ls = System.getProperty("line.separator");
+		while( ( line = reader.readLine() ) != null ) {
+			text.append( line );
+			text.append( ls );
+		}
+		text.deleteCharAt(text.length()-1);
+
+		Pattern pattern = Pattern.compile("\\d+");
+		Matcher matcher = pattern.matcher(text);
+
+		int indexMultiplierOne0=0;
+		int indexMultiplierOne1=0;
+		int indexMultiplierTwo0=0;
+		int indexMultiplierTwo1=0;
+		boolean iteration = true;
+		while (matcher.find()) {
+			if (iteration) {
+				indexMultiplierOne0 = matcher.start();
+				indexMultiplierOne1 = matcher.end();
+				iteration = false;
+				continue;
+			}
+			indexMultiplierTwo0 = matcher.start();
+			indexMultiplierTwo1 = matcher.end();
+		}
+	    double multiplier0 =  Double.parseDouble(text.substring(indexMultiplierOne0, indexMultiplierOne1));
+	    double multiplier1 =  Double.parseDouble(text.substring(indexMultiplierTwo0, indexMultiplierTwo1));
 	    System.out.println(multiplier0/(1/multiplier1));
     }
 }
